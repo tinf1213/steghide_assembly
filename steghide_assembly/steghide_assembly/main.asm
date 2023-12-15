73447f5b547b1127ea152eb06779c6b120a6d94b
Tinf <ericding1213@gmail.com>
2023-12-15 14:25:51 +0800

    add: path read from terminal

--- steghide_assembly/steghide_assembly/main.asm
+++ steghide_assembly/steghide_assembly/main.asm
@@ -4,12 +4,17 @@ INCLUDE file2byte.inc ; Include the file containing the prototype
 .data
 filename BYTE "C:\steghide_assembly\steghide_assembly\files\test.bmp", 0
 errMsg BYTE "Cannot create file", 0
+file_path BYTE 256 dup (?)
 
 .code
 main PROC
-    ;mov edx, OFFSET file_path
-    ;call ReadString
-    INVOKE File2Byte, addr filename
+    mov edx, OFFSET file_path
+    mov ecx, LENGTHOF file_path-1
+    call ReadString
+    ;mov ecx, LENGTHOF file_path
+    ;call WriteString
+    INVOKE File2Byte, ADDR file_path
+    
     ret
 main ENDP
 
