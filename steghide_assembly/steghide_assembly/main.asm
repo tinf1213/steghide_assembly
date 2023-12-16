dde92fdd43839f88d1d9ccce802b4cbdb8ca3aed
NelsonKCT <104752032+NelsonKCT@users.noreply.github.com>
2023-12-16 23:34:58 +0800

    add:CLI介面

--- steghide_assembly/steghide_assembly/main.asm
+++ steghide_assembly/steghide_assembly/main.asm
@@ -22,18 +22,41 @@ fileType byte 2
 unHideFile byte 1000 DUP(?);
 unhideFileLen dword ?
 unHideFileType byte ?
-
+;
+inputBuffer BYTE 256 DUP(?) ;Used to store user input
+prompt BYTE "Enter 'e' to encrypt or 'd' to decrypt: ", 0
+errorMsg BYTE "Invalid input. Please enter 'e' or 'd'.", 0
+encryptMsg BYTE "Enter the path of the image file: ", 0
+textMsg BYTE "Enter the path of the text file to hide: ", 0
+decryptMsg BYTE "Enter the path of the encrypted image file: ", 0
+;
 startFrom dword 82h
 
 
 
 .code
 main PROC
+    mov edx, OFFSET prompt
+    call WriteString
+
+    ; Wait for user input
+    mov edx, OFFSET inputBuffer
+    mov ecx, SIZEOF inputBuffer
+    call ReadString
+
+    ; Check user input
+    mov al, [inputBuffer]
+    cmp al, 'e'
+    je  encryptFile
+    cmp al, 'd'
+    je  decryptFile
+    jmp invalidInput
+encryptFile:
+    mov edx, OFFSET encryptMsg
+    call WriteString
     mov edx, OFFSET file_path
     mov ecx, LENGTHOF file_path-1
     call ReadString
-    ;mov ecx, LENGTHOF file_path
-    ;call WriteString
     INVOKE File2Byte, ADDR file_path
     mov esi, eax
     mov ecx, ebx
@@ -41,13 +64,12 @@ main PROC
     mov edi, OFFSET buffer_showBmp
     rep movsb
     mov showBmpPtr, OFFSET buffer_showBmp
-    
-
+    mov edx, OFFSET textMsg
+    call WriteString
     mov edx, OFFSET file_path
     mov ecx, LENGTHOF file_path-1
     call ReadString
-    ;mov ecx, LENGTHOF file_path
-    ;call WriteString
+    
     INVOKE File2Byte, ADDR file_path
     mov esi, eax
     mov ecx, ebx
@@ -64,9 +86,22 @@ main PROC
     invoke HideTheFile , showBmpPtrTemp, hideFilePtr, showBmpLen, hideFileLen, fileType
     ;;;;;;;;;寫檔加在這裡;;;;;;;;;
     invoke byte2file, showBmpPtr ,showBmpLen, fileType, 0
-
     ; 到這行是加密
-    
+    jmp exitProgram
+
+decryptFile:
+    mov edx, OFFSET decryptMsg
+    call WriteString
+    mov edx, OFFSET file_path
+    mov ecx, LENGTHOF file_path-1
+    call ReadString
+    INVOKE File2Byte, ADDR file_path
+    mov esi, eax
+    mov ecx, ebx
+    mov showBmpLen, ebx
+    mov edi, OFFSET buffer_showBmp
+    rep movsb
+    mov showBmpPtr, OFFSET buffer_showBmp
     ; 這行以下是解密
     mov eax, showBmpPtr
     add eax, startFrom
@@ -79,7 +114,16 @@ main PROC
     ; 到這行是解密
     
     invoke byte2file, offset unHideFile, unhideFileLen, unHideFileType, 1;寫檔
+    jmp exitProgram
 
+invalidInput:
+    mov edx, OFFSET errorMsg
+    call WriteString
+    jmp exitProgram
+
+exitProgram:
+    ; Your program exit code here
+    exit
 
     ret
 main ENDP
